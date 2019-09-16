4a375bf713d460b2e2c9f13fefa10f9908475a7e
Abdelrahman Awad <logaretm1@gmail.com>
2019-09-16 14:17:26 +0200

    refactor: removed shared state in geolocation API

--- src/Geolocation.ts
+++ src/Geolocation.ts
@@ -1,78 +1,44 @@
-import { reactive, toRefs } from '@vue/composition-api';
+import { reactive, toRefs, onMounted, onUnmounted } from '@vue/composition-api';
 
-interface GeolocationProps {
-  coords?: Coordinates;
-  locatedAt?: Date;
-  error?: string;
-}
-
-let STATE: ReturnType<typeof initState>;
-function initState() {
-  const data: GeolocationProps = {
+export function useGeolocation(options: PositionOptions) {
+  const data: {
+    error: string;
+    coords?: Position['coords'];
+    locatedAt?: Date;
+  } = {
     coords: undefined,
     locatedAt: undefined,
     error: ''
   };
 
-  return reactive(data);
-}
+  const state = reactive(data);
 
-function updatePosition(position: Position) {
-  STATE.locatedAt = new Date(position.timestamp);
-  const coords = position.coords;
-  STATE.coords = {
-    accuracy: coords.accuracy,
-    altitude: coords.altitude,
-    altitudeAccuracy: coords.altitudeAccuracy,
-    heading: coords.heading,
-    latitude: coords.latitude,
-    longitude: coords.longitude,
-    speed: coords.speed
-  };
-}
-
-function onError(err: PositionError) {
-  if (err.code === 1) {
-    console.log('Permission denied!');
+  function updatePosition(position: Position) {
+    state.locatedAt = new Date(position.timestamp);
+    const coords = position.coords;
+    state.coords = {
+      accuracy: coords.accuracy,
+      altitude: coords.altitude,
+      altitudeAccuracy: coords.altitudeAccuracy,
+      heading: coords.heading,
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      speed: coords.speed
+    };
   }
 
-  if (err.code === 2) {
-    console.log('Position unavailable!');
-  }
-
-  if (err.code === 3) {
-    console.log('Timeout!');
-  }
-
-  // TODO: Handle error.
-  console.log('Unsupported!');
-  STATE.error = err.message;
-}
-
-function locate() {
-  return new Promise((resolve, reject) => {
-    if (!('geolocation' in navigator)) {
-      STATE.error = 'Geolocation API is not supported';
-      return;
+  let watcher: number;
+  onMounted(() => {
+    if ('geolocation' in navigator) {
+      watcher = window.navigator.geolocation.watchPosition(updatePosition, undefined, options);
     }
-
-    window.navigator.geolocation.getCurrentPosition(
-      pos => {
-        updatePosition(pos);
-        resolve(pos);
-      },
-      err => {
-        onError(err);
-        reject(err);
-      }
-    );
   });
-}
 
-export function useGeolocation() {
-  if (!STATE) {
-    STATE = initState();
-  }
+  onUnmounted(() => {
+    if (watcher) {
+      window.navigator.geolocation.clearWatch(watcher);
+    }
+  });
 
-  return { ...toRefs(STATE), locate };
+  return { ...toRefs(state) };
 }
