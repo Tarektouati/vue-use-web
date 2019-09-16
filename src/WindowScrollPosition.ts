e085c1ee478ec4afcb5f6d276c7792d206abf96d
Abdelrahman Awad <logaretm1@gmail.com>
2019-09-16 18:16:21 +0200

    fix: use unmounted to prepopulate the data

--- src/WindowScrollPosition.ts
+++ src/WindowScrollPosition.ts
@@ -1,4 +1,4 @@
-import { onMounted, onUnmounted, reactive, toRefs } from '@vue/composition-api';
+import { onMounted, onUnmounted, reactive, toRefs, onBeforeMount } from '@vue/composition-api';
 import { throttle } from './utils';
 
 interface WindowScrollOptions {
@@ -8,17 +8,23 @@ interface WindowScrollOptions {
 export function useWindowScrollPosition(options: WindowScrollOptions = { throttleMs: 100 }) {
   const state = reactive({ x: 0, y: 0 });
 
-  const setScrollPos = throttle(options.throttleMs, function onScroll() {
+  function setScrollPos() {
     state.x = window.pageXOffset;
     state.y = window.pageYOffset;
+  }
+
+  const onScroll = throttle(options.throttleMs, setScrollPos);
+
+  onBeforeMount(() => {
+    setScrollPos();
   });
 
   onMounted(() => {
-    window.addEventListener('scroll', setScrollPos, { passive: true });
+    window.addEventListener('scroll', onScroll, { passive: true });
   });
 
   onUnmounted(() => {
-    window.removeEventListener('scroll', setScrollPos);
+    window.removeEventListener('scroll', onScroll);
   });
 
   return {
