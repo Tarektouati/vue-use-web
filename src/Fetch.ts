f8283d3f11180fd121bb34a21c5df3ef6ff12e8d
Abdelrahman Awad <logaretm1@gmail.com>
2019-10-09 01:19:26 +0200

    feat: fetch with abort implementation

--- src/Fetch.ts
+++ src/Fetch.ts
@@ -1,29 +1,77 @@
 import { reactive, toRefs, onMounted } from '@vue/composition-api';
 
 export function useFetch(url: RequestInfo, options: RequestInit) {
-  const state = reactive({
+  const stateDefs: {
+    blob: Blob | null;
+    json: any;
+    text: string;
+    isLoading: boolean;
+    success: boolean;
+    error: boolean;
+    cancelled: boolean;
+  } = {
     isLoading: false,
     success: false,
     error: false,
-    response: null
-  });
+    cancelled: false,
+    json: null,
+    blob: null,
+    text: ''
+  };
 
-  onMounted(() => {
-    window
-      .fetch(url, options)
+  const state = reactive(stateDefs);
+
+  let signal: AbortSignal | undefined;
+  let controller: AbortController;
+  if (typeof AbortController !== 'undefined') {
+    controller = new AbortController();
+    signal = controller.signal;
+  }
+
+  function execute() {
+    state.isLoading = true;
+
+    return window
+      .fetch(url, { signal, ...options })
       .then(res => {
         state.success = res.ok;
         state.error = !res.ok;
         state.isLoading = false;
 
-        return res.json();
+        return Promise.all([res.clone().text(), res.clone().blob(), res.json()]);
       })
-      .then(json => {
-        state.response = json;
+      .then(([text, blob, json]) => {
+        // Graceful degradation for cancellation.
+        if (state.cancelled) {
+          return;
+        }
+
+        state.text = text;
+        state.blob = blob;
+        state.json = json;
+      })
+      .catch(() => {
+        state.error = true;
+        state.isLoading = false;
       });
-  });
+  }
+
+  onMounted(execute);
+
+  function cancel() {
+    if (state.success) {
+      return;
+    }
+
+    state.cancelled = true;
+    if (controller) {
+      controller.abort();
+    }
+  }
 
   return {
-    ...toRefs(state)
+    ...toRefs(state),
+    cancel,
+    execute
   };
 }
