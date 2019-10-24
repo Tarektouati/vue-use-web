f55512d0bb692b76095fc7f062263f9b613ad4da
Alexander von Studnitz <alexander.von.studnitz@gmail.com>
2019-10-24 14:55:15 +0200

    Add event listener helpers

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { onMounted, onUnmounted } from '@vue/composition-api';
+
 export function throttle<T extends (...args: any[]) => void>(wait: number, fn: T): T {
   if (wait === 0) {
     return fn;
@@ -17,3 +19,25 @@ export function throttle<T extends (...args: any[]) => void>(wait: number, fn: T
     timeout = setTimeout(later, wait) as any;
   }) as T;
 }
+
+export const hasWindow = typeof window !== 'undefined';
+
+export function useEventListener(
+  element: Document | Window | Element,
+  type: string,
+  listener: EventListenerOrEventListenerObject
+) {
+  onMounted(() => {
+    element.addEventListener(type, listener);
+  });
+
+  onUnmounted(() => {
+    element.removeEventListener(type, listener);
+  });
+}
+
+export const useDocumentEventListener = (type: string, listener: EventListenerOrEventListenerObject) =>
+  useEventListener(document, type, listener);
+
+export const useWindowEventListener = (type: string, listener: EventListenerOrEventListenerObject) =>
+  useEventListener(window, type, listener);
