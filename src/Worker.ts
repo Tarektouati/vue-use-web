4ee230762d1387eca405977af8f1857ac49fc931
Ilya Zhuravlev <ilya.zhurik@gmail.com>
2019-10-28 16:58:05 -0400

    feat: add useWorker.ts (#26)
    * created Worker.ts
    
    * corrected Worker.t functionality
    
    * fixed requested changes
    
    * renamed message property to data in Worker.ts

---
+++ src/Worker.ts
@@ -0,0 +1,36 @@
+import { ref, onMounted, onUnmounted, Ref } from '@vue/composition-api';
+
+export function useWorker(url: string) {
+  let data: Ref<any> = ref(null); 
+  let worker: Worker;
+  
+  const post: typeof worker.postMessage = function post(val: any) {
+    if (!worker) return;
+
+    worker.postMessage(val);
+  };
+
+  const terminate: typeof worker.terminate = function terminate() {
+    if (!worker) return;
+
+    worker.terminate();
+  };
+
+  onMounted(() => {
+    worker = new Worker(url);
+
+    worker.onmessage = (e: MessageEvent) => {
+      data.value = e.data;
+    };
+  });
+
+  onUnmounted(() => {
+    worker.terminate();
+  });
+
+  return {
+    data,
+    post,
+    terminate
+  };
+}
