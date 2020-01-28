5c78ee451efb8be6bc48b70ab6a5f9f8e77d1327
Rijk van Zanten <rijkvanzanten@me.com>
2020-01-28 10:29:44 -0500

    Loosen typing in useEventListener
    This allows you to use a more specific Event type (like MouseEvent) in the event handler.

--- src/EventListener.ts
+++ src/EventListener.ts
@@ -1,20 +1,20 @@
 import { onMounted, onBeforeUnmount, Ref, isRef } from '@vue/composition-api';
 
-export function useEventListener<T extends EventTarget>(
+export function useEventListener<T extends EventTarget, E extends Event>(
   target: T | Ref<T>,
   type: string,
-  handler: EventListener,
+  handler: (this: T, evt: E) => void,
   options?: AddEventListenerOptions
 ) {
   onMounted(() => {
     const t = isRef(target) ? target.value : target;
 
-    t.addEventListener(type, handler, options);
+    t.addEventListener(type, handler as (evt: Event) => void, options);
   });
 
   onBeforeUnmount(() => {
     const t = isRef(target) ? target.value : target;
 
-    t.removeEventListener(type, handler, options);
+    t.removeEventListener(type, handler as (evt: Event) => void, options);
   });
 }
